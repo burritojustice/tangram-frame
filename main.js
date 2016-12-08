23dafa51aa9bc0fc88622fbeefbd18f5d281e2cf
burritojustice <john@mapzen.com>
2016-12-08 14:50:53 -0800

    Revert "merge to revert fitbounds"

--- main.js
+++ main.js
@@ -27,8 +27,6 @@ function parseQuery (qstr) {
 var map, scene, hash, query, scene_url;
 var minz = 1;
 var maxz = 22;
-var maxbounds = [[-90,0],[90,180]];
-var fitbounds = [[],[]];
 
 load = (function load() {
     if (detects.webgl === false) {
@@ -61,23 +59,9 @@ load = (function load() {
     if (query.maxz) {
         maxz = query.maxz;
     }
-    if (query.maxbounds) {
-        var max_sw_ne = query.maxbounds;
-        console.log("&max: " + max_sw_ne);
-        var a = max_sw_ne.split(',');
-        console.log("split: " + a);
-        maxbounds = [[a[0],a[1]],[a[2],a[3]]];
-        console.log("maxbounds: " + maxbounds);
+    if (query.maxBounds) {
+        maxbounds = query.maxBounds;
     }
-    if (query.fitbounds) {
-        var fit_sw_ne = query.fitbounds;
-        console.log("&fit: " + fit_sw_ne);
-        var b = fit_sw_ne.split(',');
-        console.log("split: " + b);
-        fitbounds = [[b[0],b[1]],[b[2],b[3]]];
-        console.log("fitbounds: " + fitbounds);
-    }    
-    
 
     if (scene_lib.indexOf("/") > -1) {
         // assume it's a full path
@@ -264,7 +248,6 @@ function initMap() {
             "minZoom": minz,
             "maxZoom": maxz,
             "maxBounds": maxbounds,
-            "fitBounds": fitbounds,
             }
         );
 
